32a64241eb4a595a9224c5b3342652ad23750d82
Navneet Golwalkar <ngolwalkar@yahoo.com>
2026-04-01 19:22:40 +0530

    Header and footer structure created.

--- wp-content/themes/zenctuary-theme-starter/inc/setup.php
+++ wp-content/themes/zenctuary-theme-starter/inc/setup.php
@@ -4,6 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 add_action( 'after_setup_theme', 'zenctuary_theme_setup' );
 function zenctuary_theme_setup(): void {
+	load_theme_textdomain( 'zenctuary', get_template_directory() . '/languages' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'editor-styles' );
